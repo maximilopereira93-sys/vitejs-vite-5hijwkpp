53088ad5b5438b23bc48de2fe41eb2088e1aba51
maximilopereira93-sys <maximilopereira93@gmail.com>
2026-09-11 17:11:50 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sistema de Gestão Escolar Infantil (SGEI)</h1>
+      <p>Bem-vindo! Esta será a interface do sistema.</p>
+    </div>
+  );
+}
+
+export default App;
